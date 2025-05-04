9eaebaf3bc09a8492c7a5d6464fa96e3137e7409
coresyt <corejsyt2011@gmail.com>
2025-05-04 11:40:07 -0600

    fix(Root): Change mode for the plugin **typescript-eslint**

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
+  tseslint.configs.strict,
   ...prettier,
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
