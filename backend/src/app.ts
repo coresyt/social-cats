929c53aa2abe1a9b57eb08723e71b2a82b551d17
coresyt <corejsyt2011@gmail.com>
2025-05-18 10:07:41 -0600

    feat(backend): Create routes for posts
    Create the after routes:
    
      - GET /post/list & /post/list?q=Anywhere
    
      - Get /post/:id
    
      - POST
    
    /post/create
    
      - POST /post/update/:id
    
      - POST /post/like/:id
    
      - DELETE /post/unlike/:id
    
    BREAKING CHANGES:
    
    Add authentication all routes post, in features changes add all routes!!!

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import userRouter from './routes/user.route'
+import postRouter from './routes/post.route'
 
 const app = express()
 app.use(express.json())
@@ -9,6 +10,6 @@ app.use(express.urlencoded())
 app.use(morgan('dev'))
 app.use(cors({ origin: '*' }))
 
-app.use('/api/', userRouter)
+app.use('/api/', userRouter, postRouter)
 
 export default app
---
+++ backend/src/routes/post.route.ts
@@ -0,0 +1,19 @@
+import postController from '../controllers/post.controller'
+import { Router } from 'express'
+import checkAuthentication from '../middlewares/authentication'
+
+const postRouter = Router()
+
+postRouter.get('/post/list', postController.postsList)
+
+postRouter.get('/post/:postId', postController.postId)
+
+postRouter.post('/post/create/', checkAuthentication, postController.postCreate)
+
+postRouter.post('/post/update/:postId', checkAuthentication, postController.postUpdate)
+
+postRouter.post('/post/like/:postId', checkAuthentication, postController.postLike)
+
+postRouter.delete('/post/unlike/:postId', checkAuthentication, postController.postUnlike)
+
+export default postRouter
